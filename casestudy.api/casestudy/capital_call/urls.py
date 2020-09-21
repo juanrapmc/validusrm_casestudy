36d7abf1e20f8cd730fe2108e623b1d9b45dd3e8
rap-rog <juanrapmcornejo@gmail.com>
2020-09-21 15:25:05 +0100

    Added urls and views for the capital call service

---
+++ casestudy.api/casestudy/capital_call/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from capital_call import views
+
+
+router = DefaultRouter()
+router.register(r'funds', views.FundViewSet)
+router.register(r'fundcommitments', views.FundCommitmentViewSet)
+router.register(r'fundcalls', views.FundCallViewSet)
+router.register(r'fundinvestments', views.FundCallViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- casestudy.api/casestudy/capital_call/views.py
+++ casestudy.api/casestudy/capital_call/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from capital_call.models import Fund, FundCall, FundCommitment, FundInvestment
+from capital_call.serializers import (
+    FundSerializer,
+    FundCallSerializer,
+    FundCommitmentSerializer,
+    FundInvestmentSerializer
+)
 
-# Create your views here.
+class FundViewSet(viewsets.ModelViewSet):
+    queryset = Fund.objects.all()
+    serializer_class = FundSerializer
+
+
+class FundCommitmentViewSet(viewsets.ModelViewSet):
+    queryset = FundCommitment.objects.all()
+    serializer_class = FundCommitmentSerializer
+
+
+class FundCallViewSet(viewsets.ModelViewSet):
+    queryset = FundCall.objects.all()
+    serializer_class = FundCallSerializer
+
+
+class FundInvestmentViewSet(viewsets.ModelViewSet):
+    queryset = FundInvestment.objects.all()
+    serializer_class = FundInvestmentSerializer
