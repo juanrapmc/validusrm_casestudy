34b76e319fad3b1aba2fc7c5de1d640322ab844a
rap-rog <juanrapmcornejo@gmail.com>
2020-09-21 15:24:21 +0100

    Added model serializers

---
+++ casestudy.api/casestudy/capital_call/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from capital_call import models
+
+
+class FundSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Fund
+        fields = '__all__'
+
+
+class FundCommitmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.FundCommitment
+        fields = '__all__'
+
+
+class FundCallSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.FundCall
+        fields = '__all__'
+
+
+class FundInvestmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.FundInvestment
+        fields = '__all__'
