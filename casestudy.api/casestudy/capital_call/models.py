3792f101ab5eed5b4d7971f7fccd4e5aa9271dfa
rap-rog <juanrapmcornejo@gmail.com>
2020-09-21 12:11:07 +0100

    Added capital call models

--- casestudy.api/casestudy/capital_call/models.py
+++ casestudy.api/casestudy/capital_call/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Funds(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class FundCommitment(models.Model):
+    fund_id = models.ForeignKey(Funds, on_delete=models.CASCADE, related_name='fund_commitments')
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=999, decimal_places=2)
+    drawn_amount = models.DecimalField(max_digits=999, decimal_places=2, default=0)
+
+
+class FundCall(models.Model):
+    date = models.DateField()
+    investment_name = models.CharField(max_length=255)
+    capital_requirement = models.DecimalField(max_digits=999, decimal_places=2)
+
+
+class FundInvestment(models.Model):
+    call_id = models.ForeignKey(FundCall, on_delete=models.CASCADE, related_name='call_investments')
+    fund_id = models.ForeignKey(Funds, on_delete=models.CASCADE, related_name='fund_investments')
+    commitment_id = models.IntegerField()
+    investment_amount = models.DecimalField(max_digits=999, decimal_places=2)
