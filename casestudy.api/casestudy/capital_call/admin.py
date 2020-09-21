f3c12ee2a7ca6284f386020098fd885115e126d8
rap-rog <juanrapmcornejo@gmail.com>
2020-09-21 12:39:03 +0100

    Added capitalcall models to admin

--- casestudy.api/casestudy/capital_call/admin.py
+++ casestudy.api/casestudy/capital_call/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from capital_call import models
+
+admin.site.register(models.Funds)
+admin.site.register(models.FundCommitment)
+admin.site.register(models.FundCall)
+admin.site.register(models.FundInvestment)
