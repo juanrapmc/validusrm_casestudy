cf5e969add894673bc1d9c19c6943106f598bd03
rap-rog <juanrapmcornejo@gmail.com>
2020-09-21 12:10:42 +0100

    Added app to settings

--- casestudy.api/casestudy/casestudy/settings.py
+++ casestudy.api/casestudy/casestudy/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 PROJECT_APPS = [
-    'capitalcall.apps.CapitalCallConfig',
+    'capital_call.apps.CapitalCallConfig',
 ]
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
