776b7734cdb82888b26c1d0efe244d8d915ec1d4
rap-rog <juanrapmcornejo@gmail.com>
2020-09-22 16:37:31 +0100

    Fixed url mapping

--- casestudy.api/casestudy/capital_call/urls.py
+++ casestudy.api/casestudy/capital_call/urls.py
@@ -7,7 +7,7 @@ router = DefaultRouter()
 router.register(r'funds', views.FundViewSet)
 router.register(r'fundcommitments', views.FundCommitmentViewSet)
 router.register(r'fundcalls', views.FundCallViewSet)
-router.register(r'fundinvestments', views.FundCallViewSet)
+router.register(r'fundinvestments', views.FundInvestmentViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
