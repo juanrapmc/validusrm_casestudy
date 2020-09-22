8d9d8903f3d4ea157e90ac866ab5733cc28f0988
rap-rog <juanrapmcornejo@gmail.com>
2020-09-22 19:20:18 +0100

    Added logic to compute drawdown

--- casestudy.web/src/views/CapitalCallNew.vue
+++ casestudy.web/src/views/CapitalCallNew.vue
@@ -1,15 +1,20 @@
 <template>
   <CapitalCallBase>
+    <template v-slot:content-left>
+      <CallCalculator />
+    </template>
   </CapitalCallBase>
 </template>>
 <script>
 // @ is an alias to /src
 import CapitalCallBase from '@/layouts/Base.vue'
+import CallCalculator from '@/components/CallCalculator.vue'
 
 export default {
   name: 'CapitalCallNew',
   components: {
-    CapitalCallBase
+    CapitalCallBase,
+    CallCalculator,
   }
 }
 </script>
